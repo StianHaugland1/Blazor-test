00db5aae30928eccbb38ece45f4998ef366755a1
Stian Haugland <stian.haugland@instech.no>
2024-09-16 14:11:14 +0200

    Add common project

--- BlazorIntAuto.Client/wwwroot/js/site.js
+++ BlazorIntAuto.Client/wwwroot/js/site.js
@@ -32,6 +32,7 @@
 // }
 let html5QrCode
 let cameraId
+let configUI
 
 function setupQrCodeScanner() {
   Html5Qrcode.getCameras().then(devices => {
@@ -39,15 +40,23 @@ function setupQrCodeScanner() {
      * devices would be an array of objects of type:
      * { id: "id", label: "label" }
      */
+    configUI = {
+      fps: 10,
+      videoConstraints: {
+      },
+      qrbox: (w, h) => {
+          return {
+              width: 250,
+              height: 250,
+          }
+      },
+   }
     if (devices && devices.length) {
       cameraId = devices[0].id;
       html5QrCode = new Html5Qrcode(/* element id */ "reader");
       html5QrCode.start(
         cameraId, 
-        {
-          fps: 10,    // Optional, frame per seconds for qr code scanning
-          qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
-        },
+        configUI,
         (decodedText, decodedResult) => {
           // console.log("decodedText", decodedText);
           DotNet.invokeMethodAsync('BlazorIntAuto.Client', 'ReceiveDecodedText', decodedText);
@@ -79,10 +88,7 @@ function startQrCodeScanner() {
   }
   html5QrCode.start(
     cameraId, 
-    {
-      fps: 10,    // Optional, frame per seconds for qr code scanning
-      qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
-    },
+    configUI,
     (decodedText, decodedResult) => {
       console.log("decodedText", decodedText);
       // do something when code is read
